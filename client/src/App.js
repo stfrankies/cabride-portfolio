d5f17d02994e73ce2b32eefce107458dad361ddb
Frank Tunde <frankie@Frankie.local>
2023-01-12 19:47:44 +0200

    init backend server

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Login from './components/Login';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cab Ride</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
