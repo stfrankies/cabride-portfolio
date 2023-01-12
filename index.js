9a21247b1b953a6fbb4312b8c24d9c5f3555d7d5
Frank Tunde <frankie@Frankie.local>
2023-01-12 20:13:39 +0200

    add mock data structure

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
 const http = require('http')
 
+const rides = [
+    {
+        id: 1,
+        start: "ita-keskus",
+        destination: "iso-omena",
+        starttime: "12.00",
+        stoptime: "12.30",
+        payment: "visa card",
+        amount: "50€",
+        km: 15
+    },
+    {
+        id: 2,
+        start: "keskusta",
+        destination: "malmi",
+        starttime: "12.00",
+        stoptime: "12.30",
+        payment: "visa card",
+        amount: "50€",
+        km: 15
+    },
+    {
+        id: 3,
+        start: "hakaniemi",
+        destination: "kamppi",
+        starttime: "12.00",
+        stoptime: "12.30",
+        payment: "visa card",
+        amount: "50€",
+        km: 15
+    },
+    {
+        id: 4,
+        start: "asema-aukio",
+        destination: "espoo keskusta",
+        starttime: "12.00",
+        stoptime: "12.30",
+        payment: "visa card",
+        amount: "50€",
+        km: 15
+    },
+]
+
 const app = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain' })
-    res.end('hello world')
+    res.end(JSON.stringify(rides))
 })
 
 const PORT = 3001
